0028f3c44eee042c6021e3faf6f21c9f9b68e448
xuxiao <542761360@qq.com>
2025-12-06 17:48:59 +0800

    feat: chore: 优化 Rspeedy 配置（静态资源命名与 Banner 注释插件）

--- lynx.config.ts
+++ lynx.config.ts
@@ -15,7 +15,12 @@ export default defineConfig(() => {
       pluginReactLynx(),
       pluginTypeCheck(),
     ],
-
+   // 在这里加 output 配置
+   output: {
+    filename: {
+      image: '[name].xuxiao.[contenthash:8][ext]',
+    },
+  },
     // 开发环境代理配置
     server: {
       proxy: {
@@ -23,6 +28,21 @@ export default defineConfig(() => {
           target: 'http://localhost:4000',
           changeOrigin: true,
         },
+      },
+    },
+    tools: {
+      rspack(config, { rspack }) {
+        config.plugins = config.plugins || []
+
+        config.plugins.push(
+          new rspack.BannerPlugin({
+            banner: [
+              '作者：徐潇',
+              '完成功能：开发注释插入功能',
+            ].join('\n'),
+            // 不写 raw，默认会包成块注释 /*! ... */
+          }),
+        )
       },
     },
   }
